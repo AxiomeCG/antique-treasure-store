8c1215df7067f8758bd1a5e291b77ff8a1f9ad6a
Adam N <18304870+0xAxiome@users.noreply.github.com>
2023-02-04 20:30:08 +0100

    feat: core desktop view implemented

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,36 @@ function App() {
       <ThreeViewer/>
       <div className="scroll-container">
         <section className="section-1">
-          Section 1
+          <div className="section-1__wrapper">
+            <h1>Antique Treasures</h1>
+            <h2>Passing down the love of antiques from generation to generation</h2>
+            <div className="section-1__button-container">
+              <button className="section-1__cta">Find yours today</button>
+              <button className="section-1__info">Who are we ?</button>
+            </div>
+          </div>
         </section>
         <section className="section-2">
-          Section 2
+
+          <div className="section-2__wrapper">
+            <h1>Uncover the Beauty of Antiques</h1>
+            <h2>Experience the Charm and History of Our Hand-Selected Antique Collection</h2>
+          </div>
+          <div className="svg-container">
+            <svg id="shape" viewBox="0 0 1280 1024" fill="none" xmlns="http://www.w3.org/2000/svg"
+                 preserveAspectRatio="xMidYMin slice">
+              <path
+                d="M-43 0.499995C-43 0.499995 286.701 25.1889 726.844 145.425C1240.51 285.745 1376.23 466.5 1376.23 466.5L1376.23 1023.5C1376.23 1023.5 1042.5 779.933 671 675C299.5 570.067 -43 622.627 -43 622.627L-43 0.499995Z"
+                fill="#C1CCF1"/>
+            </svg>
+          </div>
+
         </section>
         <section className="section-3">
-          Section 3
+          <div className="section-3__wrapper">
+            <h1>Celebrate Craftsmanship</h1>
+            <h2>Discover the Artistry and Attention to Detail of Antiques Made with Unsurpassed Skill and Precision</h2>
+          </div>
         </section>
       </div>
     </>
--- src/ThreeViewer.tsx
+++ src/ThreeViewer.tsx
@@ -1,6 +1,6 @@
-import { Canvas, useThree } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { PerspectiveCamera, useGLTF } from "@react-three/drei";
-import React, { useEffect, useLayoutEffect, useRef } from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import * as THREE from "three";
 import { Euler, Quaternion, Vector3 } from "three";
 import gsap from "gsap";
@@ -21,12 +21,12 @@ export const cameraKeyframes: CameraKeyframes[] = [
     rotation: new Euler().setFromQuaternion(new Quaternion(-0.05, 0.43, 0.02, 0.9)),
   },
   {
-    position: new Vector3(0.4, 3.4, 1.1),
+    position: new Vector3(0.5, 3, 1),
     target: new Vector3(0.87, -1.27, 0.69),
     rotation: new Euler().setFromQuaternion(new Quaternion(-0.59, -0.33, -0.29, 0.7)),
   },
   {
-    position: new Vector3(0.7450892432264768, -1.171423004129647,  1.6672241164365516),
+    position: new Vector3(-0.7450892432264768, -1.171423004129647, 1.6672241164365516),
     target: new Vector3(-2.79, -0.13, 0.49),
     rotation: new Euler().setFromQuaternion(new Quaternion(0.04968318113661581, -0.028031887588781116, -0.028031887588781116, 0.8695191141477744)),
   },
@@ -47,31 +47,30 @@ function CameraKeyframes() {
       let scrollTriggerParams = {
         start: "top bottom",
         end: "top top",
-        markers: true,
-        toggleActions: "play pause resume reverse",
-        immediateRender: false
+        toggleActions: "play none none reverse",
+        scrub: true
       };
-     gsap.timeline( {
+      gsap.timeline({
         scrollTrigger: {
           ...scrollTriggerParams,
-          trigger: '.section-2'
+          trigger: ".section-2"
         }
       }).to(cameraRef.current.position, {
         x: cameraKeyframes[1].position.x,
         y: cameraKeyframes[1].position.y,
         z: cameraKeyframes[1].position.z,
 
-      }, 'label0' )
+      }, "label0")
         .to(cameraRef.current.rotation, {
           x: cameraKeyframes[1].rotation.x,
           y: cameraKeyframes[1].rotation.y,
           z: cameraKeyframes[1].rotation.z,
-        }, 'label0')
+        }, "label0")
 
-      gsap.timeline( {
+      gsap.timeline({
         scrollTrigger: {
           ...scrollTriggerParams,
-          trigger: '.section-3'
+          trigger: ".section-3"
         }
       })
         .to(cameraRef.current.position, {
@@ -79,13 +78,13 @@ function CameraKeyframes() {
           y: cameraKeyframes[2].position.y,
           z: cameraKeyframes[2].position.z,
 
-        },'label1')
+        }, "label1")
         .to(cameraRef.current.rotation, {
           x: cameraKeyframes[2].rotation.x,
           y: cameraKeyframes[2].rotation.y,
           z: cameraKeyframes[2].rotation.z,
 
-        }, 'label1')
+        }, "label1")
     });
 
     return () => ctx.revert(); // cleanup!
@@ -93,7 +92,8 @@ function CameraKeyframes() {
 
 
   return <>
-    <PerspectiveCamera ref={cameraRef} makeDefault fov={50} position={cameraKeyframes[0].position} rotation={cameraKeyframes[0].rotation} />
+    <PerspectiveCamera ref={cameraRef} makeDefault fov={50} position={cameraKeyframes[0].position}
+                       rotation={cameraKeyframes[0].rotation}/>
   </>;
 }
 
